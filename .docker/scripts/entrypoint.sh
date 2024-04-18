5deda217abeff2e8dfc5ec313cbd385b1f6da57d
Young Joon Lee <entelecheia@hotmail.com>
2024-04-19 04:34:57 +0900

    refactor(entrypoint): improve verbosity and permission handling

--- .docker/scripts/entrypoint.sh
+++ .docker/scripts/entrypoint.sh
@@ -1,21 +1,41 @@
 #!/bin/bash
-# Print out the environment variables
-export
-
+VERBOSE=${DOCKER_VERBOSE:-"false"}
 # add your custom commands here that should be executed every time the docker container starts
-echo "Starting docker container..."
+if [ "$VERBOSE" = "true" ]; then
+    echo "Starting docker container..."
+    echo "---"
+    # Print out the environment variables
+    env
+    echo "---"
+    echo "Fixing permissions..."
+    echo "---"
+fi
 
 ### Set the USER_UID envvar to match your user.
 # Ensures files created in the container are owned by you:
 #   docker run --rm -it -v /some/path:/invokeai -e USER_UID=$(id -u) <this image>
 # Default UID: 1000 chosen due to popularity on Linux systems. Possibly 501 on MacOS.
+USER_UID=${USER_UID:-"1000"}
+USERNAME=${USERNAME:-"app"}
+LOCAL_UID=$(id -u "$USERNAME")
+WORKSPACE_ROOT=${WORKSPACE_ROOT:-""}
+APP_INSTALL_ROOT=${APP_INSTALL_ROOT:-""}
 
-echo "Fixing permissions..."
-USER_UID=${USER_UID:-1000}
-USER=${USERNAME:-app}
-usermod -u "${USER_UID}" "${USER}"
-groupmod -g "${USER_UID}" "${USER}"
-# chown -R "$USER_UID:$USER_UID" "$WORKSPACE_ROOT"
+if [ "$USER_UID" != "$LOCAL_UID" ]; then
+    echo "Updating UID and GID to $USER_UID:$USER_UID from $LOCAL_UID:$LOCAL_UID"
+    usermod -u "$USER_UID" "$USERNAME"
+    groupmod -g "$USER_UID" "$USERNAME"
+    echo "Changing ownership of home directory to $USER_UID:$USER_UID"
+    chown -R "$USER_UID:$USER_UID" "/home/$USERNAME"
+    if [ -n "$APP_INSTALL_ROOT" ] && [ -d "$APP_INSTALL_ROOT" ]; then
+        echo "Changing ownership of $APP_INSTALL_ROOT directory to $USER_UID:$USER_UID"
+        chown -R "$USER_UID:$USER_UID" "$APP_INSTALL_ROOT"
+    fi
+    if [ -n "$WORKSPACE_ROOT" ] && [ -d "$WORKSPACE_ROOT" ]; then
+        echo "Changing ownership of workspace directory [$WORKSPACE_ROOT] to $USER_UID:$USER_UID"
+        chown -R "$USER_UID:$USER_UID" "$WORKSPACE_ROOT"
+    fi
+fi
 
 # Run the provided command
 exec gosu "${USER}" /bin/ollama "$@"
